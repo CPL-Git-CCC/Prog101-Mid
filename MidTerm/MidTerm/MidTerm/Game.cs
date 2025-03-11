36dc1ad677237cc410cc285407cce5bb165bf757
CPL-Git-CCC <lonergan01@colum.edu>
2025-03-11 23:58:29 +0000

    Add files via upload

---
+++ MidTerm/MidTerm/MidTerm/Game.cs
@@ -0,0 +1,26 @@
+﻿// Game.cs
+namespace AdventureGame
+{
+    public class Game
+    {
+        private Player _player;
+        private Bear _bear;
+        private Maze _maze;
+
+        public Game(Player player, Bear bear)
+        {
+            _player = player;
+            _bear = bear;
+            _maze = new Maze(player, bear);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"\nWelcome, {_player.Name}! Your adventure begins...");
+            _player.DisplayStats();
+            _bear.DisplayStats();
+
+            _maze.Explore();
+        }
+    }
+}
